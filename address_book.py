240092a9b81f6826b5161d5ed049167da15e2416
moshecha <109320893+moshecha@users.noreply.github.com>
2022-07-28 07:44:17 -0300

    libreta de contactos

--- address_book.py
+++ address_book.py
@@ -1,37 +1,36 @@
 import csv
 import random
 from datetime import date
+import uuid
+ID = uuid.uuid1()
 
 def fecha():
     today = date.today() #Para la fecha
     return (f'{format(today.day)}/{format(today.month)}/{(today.year)}') 
 
 def guardar_contacto (archivo,contacto):
-    with open(archivo, "a+", newline ='\n') as csvfile:
+    with open(archivo, "a+", newline ='') as csvfile:
         wr = csv.writer(csvfile, dialect='excel', delimiter=',', lineterminator='')
         wr.writerow(contacto)
         wr.writerow('\n')
-    print('Nuevo contacto Ingresado:',','.join(contacto[:-1]))#el -1 le saca el ,'\n'
+    #print('Nuevo contacto Ingresado:',','.join(contacto[:-1]))#el -1 le saca el ,'\n'
     
-def ID ():
-    Id = []
-    # Usar la libreria "uuid" para el campo ID
-    idnumeros = list('1234567890')
-    for i in range(5):
-        Id.append(random.choice(idnumeros)) #Elige numeros para el id / o .extend
-    random.shuffle(Id) #los mezcla
-    # No usar un formato para el id, solo devolver el valor de ID
-    Id = (f'Id:{"".join(Id)}')  #ordena en un str
-    return Id
+# def ID ():
+#     Id = []
+#     idnumeros = list('1234567890')
+#     for i in range(5):
+#         Id.append(random.choice(idnumeros)) #Elige numeros para el id / o .extend
+#     random.shuffle(Id) #los mezcla
+#     Id = (f'{"".join(Id)}')  #ordena en un str
+#     return Id
 
 
 
 nombre = input('Nombre y Apellido: ')
 direccion = input('Direccion: ')
-# No agregar la palabra Tel a la variable, solo usar el input
-numero = 'Tel: '+input('Telefono: ') 
+numero = input('Telefono: ') 
 
 archivo = 'addresses.csv'
-newContacto = [nombre.title(),direccion.title(),numero,fecha(),ID()]#,'\n' 
+newContacto = [nombre.title(),direccion.title(),numero,fecha(),ID]#,'\n' 
 
 guardar_contacto(archivo,newContacto)
